28d094b2fca6b908c1ddb070af202c5ffdadf579
divya-prav <divya.praveen.5215@gmail.com>
2024-02-13 20:50:20 -0600

    Added-View and clear button

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,59 +1,75 @@
-import { useState } from "react"
+import { useState } from "react";
 
+export default function Home({ data }) {
+  const [people, setPeople] = useState(data);
 
-export default function Home({data}){
+  const container = {
+    backgroundColor: "#E8C872",
+    width: "30%",
+    height: "auto",
+    margin: "3% 35%",
+    padding: "20px",
+  };
+  const buttonStyle = {
+    backgroundColor: "#1F2544",
+    padding: "5px 10px",
+    color: "white",
+    width: "100px",
+    margin: "20px",
+  };
 
-    const [people,setPeople] = useState(data)
+  const divStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  };
 
-    const container = {
-        backgroundColor:'#E8C872',
-        width:'20%',
-        height:'auto',
-        margin: '3% 40%',
-        padding:'20px 10px'
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+    borderRadius: "100%",
+    padding:'5%',
+    aspectratio:'fit'
+   
+  };
 
+  const messageButton = {
+    backgroundColor: "#1F2544",
+    padding: "10px 10px",
+    color: "white",
+    width: "100px",
+    margin: "20%",
+  };
+  function handleClick() {
+    if (people.length > 1) {
+      setPeople([]);
+    } else {
+      setPeople(data);
     }
-    const buttonStyle = {
-        backgroundColor:"#1F2544",
-        padding:'5px 10px',
-        color:'white',
-        width:'100px',
-        margin:'20px'
+  }
 
-    }
-
-    const divStyle = {
-        display :"flex",
-        flexDirection:"row",
-        justifyContent:'center',
-        
-    }
-
-    const imageStyle = {
-        width:"100px",
-        height:"100px",
-        borderRadius:'100%',
-        padding:'10px'
-    }
-   return (
+  return (
     <div style={container}>
-       <h2  style={{color:"white"}}>{data.length} Birthday Today</h2>
-       {
-        people.map((profile)=>{
-            return (
-                <div key={profile.id} style={divStyle}>
-                <img src={profile.image} style={imageStyle}/> 
-                <div>
-                <h5>{profile.name}</h5>
-                <h5>{profile.age} years</h5>
-                </div>
-                
-               </div>
-            )
-        })
-       
-       }
-        <button  style={buttonStyle} onClick={()=>setPeople([])}>{people.length > 1 ?"Clear all":"View All"}</button>
+      <h2 style={{ color: "white" }}>{data.length} Birthday Today</h2>
+      {people.map((profile) => {
+        return (
+          <div key={profile.id} style={divStyle}>
+            <div>
+            <img src={profile.image} style={imageStyle} />
+            </div>
+            <div style={{ display:'flex',flexDirection:'column' }}>
+              <p style={{padding:'0px'}}>{profile.name}</p>
+              <p>{profile.age} years</p>
+            </div>
+            <div>
+              <button style={messageButton}>message</button>
+            </div>
+          </div>
+        );
+      })}
+      <button style={buttonStyle} onClick={() => handleClick()}>
+        {people.length > 1 ? "Clear All" : "View All"}
+      </button>
     </div>
-   )
+  );
 }
